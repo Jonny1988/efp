b0a09f8de2dfdbe306ee989885d108f13b19ee24
Jonny1988 <brenner.jonathan@gmx.de>
2017-06-24 16:38:20 +0200

    first commit

---
+++ src/efp_project/core.clj
@@ -0,0 +1,9 @@
+(ns efp-project.core
+(:require [compojure.core :refer :all]
+            [org.httpkit.server :refer [run-server]])) ; httpkit is a server
+
+(defroutes myapp
+  (POST "/" [] "Hello World"))
+
+(defn -main []
+  (run-server myapp {:port 5000}))
